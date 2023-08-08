2318c5c4497a981dd05d62fc9feb54738dc72713
Waridi318 <rosenjeri7151@gmail.com>
2023-08-08 11:21:12 +0300

    Created perfomance_schema database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
 -- create a hbnb_dev_db database and hbnb_dev user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS perfomance_schema;
 USE perfomance_schema;
 GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
