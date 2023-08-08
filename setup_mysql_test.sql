b90f74edab9709f911bb44134e474dd8bdc6fd5f
Waridi318 <rosenjeri7151@gmail.com>
2023-08-08 11:25:45 +0300

    Task 3.1 and 4.1

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,6 @@
 -- create a hbnb_dev_test database and hbnb_test user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-CREATE DATABASE IF NOT EXISTS perfomance_schema;
-USE perfomance_schema;
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON 'perfomance_schema'.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
